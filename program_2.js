4dfceba5f7edf2eca14cbf57594e0ad7964b08a4
Sarwar-Rashid <sarwarrashid21@gmail.com>
2021-11-09 14:55:01 +0600

    keyword, data type in java script

---
+++ program_2.js
@@ -0,0 +1,22 @@
+
+// Keywords -> for, while, do while, if, else, else if, break, try, catch, function, var, const, let etc
+// Data type -> string, numbe, boolean, objects
+
+
+// Data type:
+// Primitive data type - String, Number (int,float,double), Boolean, Undefined, Null
+// Reference data type - Array, Object
+
+// Use typeof() to check the data type
+
+
+console.log(typeof ("Sarwar Rashid")); // Returns "string"
+console.log(typeof (129)); // Returns "number"
+console.log(typeof (true)); // Returns "boolean"
+console.log(typeof (false)); // Returns "boolean"
+console.log(typeof (x)); // Returns "undefined" (if x has no value)
+
+
+var person = { firstName: "Sarwar", lastName: "Rashid" };
+person = null; // Value is null, but type is still an object
+
