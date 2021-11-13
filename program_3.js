b4228468245e2288d2532956bfed83b64357bbc4
Sarwar-Rashid <sarwarrashid21@gmail.com>
2021-11-13 23:28:41 +0600

    how to declare variables in JavaScript

---
+++ program_3.js
@@ -0,0 +1,33 @@
+//How to declare variables in JavaScript
+//The general rules for constructing names for variables (collected from w3schools)
+
+/*3 ways to declare a JavaScript variable:
+
+1.Using var
+2.Using let
+3.Using const*/
+
+/*All JavaScript variables must be identified with unique names:
+
+1. Variable names can only contain letters (A-Z or a-z), digits (0-9), underscores (_) and dollar ($) signs
+2. Variable names must begin with a letter, an underscore (_) or a dollar sign ($)
+3. Variable names cannot contain spaces
+4. Reserved words (like JavaScript keywords) cannot be used as names
+5. Variable names are case-sensitive*/
+
+//Using var
+var myName = "Sarwar Rashid";
+var age1 = 20;
+var _lastName = "Rashid";
+var $ = 129;
+alert(myName);
+
+//Using let
+let firstName = "Sarwar";
+document.write(firstName);
+
+//Using const
+const PI = 3.1416;
+console.log(PI);
+
+
