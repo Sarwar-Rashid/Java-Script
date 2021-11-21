640299836c4248249b022d764e480ef9d0205a21
Sarwar-Rashid <sarwarrashid21@gmail.com>
2021-11-21 22:27:29 +0600

    Area of various shapes

---
+++ program_9.js
@@ -0,0 +1,18 @@
+//Area of various shapes
+
+//Rectangle area calculate 
+//Rectangle formula -> Base * Height
+var base = parseFloat(prompt("Enter base :"));
+var height = parseFloat(prompt("Enter height :"));
+
+var area = base * height;//Rectangle formula
+alert("Area : " + area);
+
+
+//Triangle area calculate 
+//Triangle formula -> Base * Height / 2
+var base = parseFloat(prompt("Enter base :"));
+var height = parseFloat(prompt("Enter height :"));
+
+var area = (base * height) / 2;//Triangle formula
+alert("Area : " + area);
